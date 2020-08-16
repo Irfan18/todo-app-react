176de086482894a1e84fd8c3169647226531d048
irfan <irfan.shaikh@adapty.com>
2020-08-17 01:15:54 +0530

    Updates

---
+++ precache-manifest.f5cfa550f5c6c332c1e46802ab1e8b5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6ff146369fa40f2f1f3e8ec3db597a5",
+    "url": "/todo-app-react/index.html"
+  },
+  {
+    "revision": "ff71b0915f1e2583705b",
+    "url": "/todo-app-react/static/css/main.d8dbf058.chunk.css"
+  },
+  {
+    "revision": "1636b6530ddebc0d9f15",
+    "url": "/todo-app-react/static/js/2.f6e35cdf.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/todo-app-react/static/js/2.f6e35cdf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff71b0915f1e2583705b",
+    "url": "/todo-app-react/static/js/main.8d9df016.chunk.js"
+  },
+  {
+    "revision": "e2beef5f42868147d46b",
+    "url": "/todo-app-react/static/js/runtime-main.3ef4b535.js"
+  }
+]);
